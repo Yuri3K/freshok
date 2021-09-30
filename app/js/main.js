ab3eb80f7b2730e1f5b90adb68154928ccdeb35a
Yuri3K <zebrawild@gmail.com>
2021-09-30 17:40:53 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function () {
 
+    $('.menu__btn, .menu__sublink').on('click', function () {
+        $('.menu__sublist').toggleClass('show');
+    });
+
+
+    $('.carousel').slick({
+        prevArrow: '<button class="slick-arrow--prev" type="button"><span class="sr-only">Предыдущий слайд</span></button>',
+        nextArrow: '<button class="slick-arrow--next" type="button"><span class="sr-only">Следующий слайд</span></button>',
+
+        // responsive: [
+        //     {
+        //         breakpoint: 1280,
+        //         settings: {
+        //             dots: true,
+        //             arrows: false,
+        //         }
+        //     },
+        // ]
+    });
 });
 
 
